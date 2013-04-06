4f3c4f7bba63295777f4ade1289b2a2fffbd821c
Ashok <ashokk@thoughtworks.com>
2013-04-07 00:22:50 +0530

    Ashok - Added JSON

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
-	def index
+	def map
+	  diseases = Disease.all
+	  @final_json = {}
+	  diseases.each do |dis|	    	    
+	    if @final_json[dis.occurence_date]
+	      @final_json[dis.occurence_date] << [dis.latitude, dis.longitude]
+	    else
+	      @final_json[dis.occurence_date] = [dis.latitude, dis.longitude]
+	    end  
+    end
+    @final_json
 	end
 end
