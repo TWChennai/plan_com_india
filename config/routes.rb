32fb86ac935560416f88dadf146c6f43d39dc3ff
Krishnaswamy Subramanian <subramk@thoughtworks.com>
2013-04-06 22:15:12 +0530

    Moving root to top in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 PlanComIndia::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
+
+  root :to => 'home#index'
+  
   ActiveAdmin.routes(self)
 
   # The priority is based upon order of creation:
@@ -51,7 +54,7 @@ PlanComIndia::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
