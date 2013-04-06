3dd68a72c10406a91eaa4c215d024b69a919fa59
Selva <k.n.selvakumar@gmail.com>
2013-04-06 18:22:24 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlanComIndia::Application.config.secret_token = '324467f7eb738efc7d336a5d99d74d50a52e2de51826933269e8379cc9ea02608543d5bd5e41baadcc3d0e0cb7311c93367e955571947d023e6b44dc5bfc3111'
