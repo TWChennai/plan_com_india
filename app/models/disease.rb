b65f927227278c13a5008abbd0d8c07090d777bb
Selva <k.n.selvakumar@gmail.com>
2013-04-07 12:23:42 +0530

    Dont geocode everytime

--- app/models/disease.rb
+++ app/models/disease.rb
@@ -1,6 +1,7 @@
 class Disease < ActiveRecord::Base
   attr_accessible :address, :latitude, :longitude, :name, :occurence_date, :patient_name, :pincode, :affect_count
   default_scope order("occurence_date asc")
+
   geocoded_by :address
-  after_validation :geocode
+  after_validation :geocode, :if => :address_changed?
 end
