dfe772f4a65a10db2b17816ad4edd9811b9b5ba8
Selva <k.n.selvakumar@gmail.com>
2013-04-07 11:59:28 +0530

    Order disease by occurence_date always

--- app/models/disease.rb
+++ app/models/disease.rb
@@ -1,5 +1,6 @@
 class Disease < ActiveRecord::Base
   attr_accessible :address, :latitude, :longitude, :name, :occurence_date, :patient_name, :pincode, :affect_count
+  default_scope order("occurence_date asc")
   geocoded_by :address
   after_validation :geocode
 end
