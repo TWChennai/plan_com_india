6e987ffe70f0c824f88dabee4b6f6baec30728c1
Selva <k.n.selvakumar@gmail.com>
2013-04-07 11:57:27 +0530

    parse date properly

--- app/controllers/disease_controller.rb
+++ app/controllers/disease_controller.rb
@@ -2,7 +2,7 @@ class DiseaseController < ApplicationController
   respond_to :json
 
   def create
-    params[:occurence_date] = Time.at(params[:occurence_date].to_i/1000) if params[:occurence_date].present?
+    params[:occurence_date] = Time.parse(params[:occurence_date]) if params[:occurence_date].present?
     disease = Disease.create! params.slice(:address, :latitude, :longitude, :name, :occurence_date, :patient_name, :pincode, :affect_count)
     render :json => disease
   end
